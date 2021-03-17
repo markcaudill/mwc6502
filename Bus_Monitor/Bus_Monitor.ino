6942d73da0486ba5ce095e81e9518822e0f5c29f
Mark Caudill <mark@mrkc.me>
2021-03-17 19:28:44 -0400

    Switch to serial. Add mnemonics.

--- Bus_Monitor/Bus_Monitor.ino
+++ Bus_Monitor/Bus_Monitor.ino
@@ -2,7 +2,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_MCP23017.h>
+#include "65C02S_Mnemonics.h"
 
+#define SERIAL_BAUD 115200
 #define CLOCK 2
 #define MCP1_ADDR 0
 #define MCP2_ADDR 1
@@ -18,9 +20,13 @@ Adafruit_MCP23017 mcp1, mcp2;
 volatile byte dataValue;
 volatile unsigned int addressValue;
 volatile boolean rwb;
+volatile boolean sync;
 volatile boolean clockTicked;
+String output;
 
 void setup() {
+  output.reserve(256);
+  Serial.begin(SERIAL_BAUD);
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.setTextColor(SSD1306_WHITE);
 
@@ -28,8 +34,9 @@ void setup() {
   mcp2.begin(MCP2_ADDR, &Wire);
 
   // Enable pullups on unused pins
-  for (int i = 10; i <= 15; i++)
+  for (int i = 10; i <= 15; i++) {
     mcp2.pullUp(i, HIGH);
+  }
 
   attachInterrupt(digitalPinToInterrupt(CLOCK), setClockTicked, RISING);
 }
@@ -42,18 +49,26 @@ void updateValues() {
   addressValue = mcp2.readGPIOAB();
   dataValue = mcp1.readGPIO(0);
   rwb = mcp1.digitalRead(8);
+  sync = mcp1.digitalRead(9);
 }
 
 void loop() {
   if (clockTicked) {
     updateValues();
     clockTicked = false;
-    char output[64];
-    sprintf(output, "%04x %c  %02x", addressValue, rwb ? 'r' : 'W', dataValue);
+    
+    output = String(addressValue, HEX);
+    output += " ";
+    output += String(rwb ? 'r' : 'W');
+    output += " ";
+    output += sync ? String(mnemonic[dataValue]) : String(dataValue, HEX);
+    
+    Serial.println(output);
+    
     display.clearDisplay();
     display.setTextSize(2);
     display.setCursor(0, 0);
-    display.println("AB   RW DB");
+    display.println(F("AB   RW DB"));
     display.println(output);
     display.display();
   }
